d07bbcaa1cfdbbf56f823e80e0f23a714f29208c
Endika Santamaria <endika.santamaria@stubhub.com>
2021-03-25 12:38:28 +0100

    changes device to bitrise device available

--- config/android.info.js
+++ config/android.info.js
@@ -1,5 +1,5 @@
 module.exports = {
-    deviceName: 'Nexus_S_API_29', // pass the udid ode device
+    deviceName: 'Pixel2', // pass the udid ode device
     //deviceName: 'Nexus_6_API_R', // pass the udid ode device
     platformVersion: '10', // pass the platform version
     app: 'StubHub-debug.apk',
