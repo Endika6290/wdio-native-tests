3f8c7755450e00848bde504b6ac375edb42fea24
Endika Santamaria <endika.santamaria@stubhub.com>
2021-03-31 14:42:49 +0200

    Changed ios version to 13.0 and iphone 11  for bitrise

--- config/ios.info.js
+++ config/ios.info.js
@@ -1,6 +1,6 @@
 module.exports = {
-    deviceName: 'iPhone 8 Plus', // pass the udid or device name
-    platformVersion: '14.0', // pass the platform version
+    deviceName: 'iPhone 11', // pass the udid or device name
+    platformVersion: '13.0', // pass the platform version
     app: 'StubHub.app',
     autoDismissAlerts:true
 };
