7d92f03b41528d2e63b6534867edb0cd33f37425
Endika Santamaria <endika.santamaria@stubhub.com>
2021-03-31 09:22:04 +0200

    Changed ios device for bitrise

--- config/ios.info.js
+++ config/ios.info.js
@@ -1,5 +1,5 @@
 module.exports = {
-    deviceName: 'iPhone 11', // pass the udid or device name
+    deviceName: 'iPhone 8 Plus', // pass the udid or device name
     platformVersion: '13.0', // pass the platform version
     app: 'StubHub.app',
     autoDismissAlerts:true
