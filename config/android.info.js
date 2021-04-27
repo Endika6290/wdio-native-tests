1441cb8edc3dc4011509db913e67ce5091220b9a
Endika Santamaria <endika.santamaria@stubhub.com>
2021-04-27 13:56:55 +0200

    changed apk name

--- config/android.info.js
+++ config/android.info.js
@@ -2,6 +2,6 @@ module.exports = {
     deviceName: 'emulator', // pass the udid ode device
     //deviceName: 'Nexus_6_API_R', // pass the udid ode device
     platformVersion: '10', // pass the platform version
-    app: 'StubHub-debug-bitrise-signed.apk',
+    app: 'StubHub-debug.apk',
     //udid:"1f7dffae"
 };
